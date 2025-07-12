07bbfb085a56699a76265bd16b93b024e6fed11a
Garvit Sharma <garvitsharma1994@gmail.com>
2025-07-13 01:43:20 +0530

    changes

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,6 +2,8 @@
 import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+console.log("API_BASE_URL", API_BASE_URL);
+
 
 export interface SentimentResponse {
   label: 'positive' | 'negative';
@@ -9,6 +11,8 @@ export interface SentimentResponse {
 }
 
 export const predictSentiment = async (text: string): Promise<SentimentResponse> => {
+    console.log("API_BASE_URL", API_BASE_URL);
+
   const response = await axios.post(`${API_BASE_URL}/predict`, { text });
   return response.data;
 };
